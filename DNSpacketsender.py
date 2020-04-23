6a85f3bb1938d90a854fabf5dd683df64d6d7636
cmoore147 <51740624+cmoore147@users.noreply.github.com>
2020-04-23 13:53:09 -0400

    updates

--- DNSpacketsender.py
+++ DNSpacketsender.py
@@ -1,5 +1,5 @@
 import socket
-
+import binascii
 
 hdr = b'\xAA\xAA\x01\x00' #Id | Control
 
@@ -19,22 +19,26 @@ msglen = b'\x05'
 hdr += msglen
 
 msg="hello" #this would be our custom payload??
+msg = b'\x68\x65\x6c\x6c\x6f'
 
 def stringTohex(msg):
     return ''.join(r'\x{02:x}'.format(ord(c)) for c in msg)
 
-
-hdr += stringTohex(msg)
+hdr += msg
 hdr += b'\x00' #marks the end of the message
 hdr += b'\x00\x01\x00\x01' # Qtype | Qclass
-
+#print(int(hdr,16))
 
 def sender(msg,address,port):
+
+
     addport = (address,port)
     sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
     try:
-        sock.send(binascii.unhexlify(hdr),addPort)
+        sock.sendto(hdr,addport)
     finally:
         sock.close()
-
+address = '192.168.3.156'
+port = 53
+sender(hdr,address,port)
